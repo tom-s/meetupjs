d61de5dd676b72daba582a5d76069bb86f920612
tom-s <thom.schell@gmail.com>
2018-03-24 18:23:32 +0100

    feat(stars): add stars

--- src/steps/opticalFlow/index.js
+++ src/steps/opticalFlow/index.js
@@ -1,68 +1,183 @@
 import React, { Component } from 'react'
-import { getDirectionalColor } from './colors'
-import './style.css'
+
+const scale = 2
+const fStep = scale
 
 class OpticalFlow extends Component {
   constructor(props) {
     super(props)
     this.flow = null
-    this.canvas = null
-    this.video = null
+    this.canvasEl = null
+    this.ocanvasEl = null
+    this.tcanvasEl = null
+    this.fcanvasEl = null
+    this.videoEl = null
+    this.containerEl = null
+
+    this.startTime = null
+    this.balls = []
   }
-  componentWillUnmount = () => {
-    this.flow.stopCapture()
-  }
+
   componentDidMount = () => {
     window.navigator.mediaDevices
       .getUserMedia({ audio: false, video: true })
       .then(this.videoReady)
       .catch(this.videoError)
-    this.flow = new window.oflow.WebCamFlow()
-    this.flow.onCalculated(this.onCapture)
-    // Starts capturing the flow from webcamera
-    this.flow.startCapture()
+  }
+
+  extractDifferences = () => {
+    const context = this.canvasEl.getContext('2d')
+    const oCtx = this.ocanvasEl.getContext('2d')
+    const tCtx = this.tcanvasEl.getContext('2d')
+
+    if (
+      this.canvasEl.width !== this.videoEl.videoWidth / scale ||
+      this.canvasEl.height !== this.videoEl.videoHeight / scale
+    ) {
+      this.canvasEl.width = this.ocanvasEl.width = this.tcanvasEl.width =
+        this.videoEl.videoWidth / scale
+      this.canvasEl.height = this.ocanvasEl.height = this.tcanvasEl.height =
+        this.videoEl.videoHeight / scale
+      this.fcanvasEl.width = this.videoEl.videoWidth
+      this.fcanvasEl.height = this.videoEl.videoHeight
+    }
+
+    try {
+      context.drawImage(
+        this.videoEl,
+        0,
+        0,
+        this.videoEl.videoWidth,
+        this.videoEl.videoHeight,
+        0,
+        0,
+        this.canvasEl.width,
+        this.canvasEl.height
+      )
+      if (this.canvasEl.width > 0 && this.canvasEl.height > 0) {
+        tCtx.drawImage(this.canvasEl, 0, 0)
+      }
+
+      var iData = context.getImageData(
+        0,
+        0,
+        this.canvasEl.width,
+        this.canvasEl.height
+      )
+      var oData = oCtx.getImageData(
+        0,
+        0,
+        this.ocanvasEl.width,
+        this.ocanvasEl.height
+      )
+    } catch (e) {
+      window.requestAnimationFrame(this.extractDifferences)
+      return
+    }
+
+    var buf8 = new Uint8ClampedArray(iData.data)
+    var oBuf8 = new Uint8ClampedArray(oData.data)
+
+    var d = this.flow.calculate(
+      buf8,
+      oBuf8,
+      this.canvasEl.width,
+      this.canvasEl.height
+    )
+
+    var currentTime = Date.now()
+    var eTime = currentTime - this.startTime
+    this.startTime = currentTime
+    var f = eTime / 16
+
+    this.updateCanvas(d, f)
+
+    oCtx.drawImage(this.tcanvasEl, 0, 0)
+
+    window.requestAnimationFrame(this.extractDifferences)
   }
 
   videoReady = stream => {
-    this.video.srcObject = stream
+    this.videoEl.srcObject = stream
+    this.startTime = Date.now()
+    this.tcanvasEl = document.createElement('canvas')
+    this.ocanvasEl = document.createElement('canvas')
+    this.fcanvasEl = document.createElement('canvas')
+    this.canvasEl.width = 0
+    this.flow = new window.oflow.FlowCalculator(fStep)
+    this.extractDifferences()
   }
 
   videoError = error => {
     console.log('navigator.getUserMedia error: ', error)
   }
 
-  onCapture = direction => {
-    // render zones
-    const context = this.canvas.getContext('2d')
-    context.clearRect(0, 0, 640, 480)
-    context.lineWidth = 15
-    direction.zones.forEach(zone => {
-      context.strokeStyle = getDirectionalColor(zone.u, zone.v)
+  updateCanvas = (d, f) => {
+    const context = this.canvasEl.getContext('2d')
+    var min = 2
+    var maxLife = 100
+    for (let j = 0; j < d.zones.length; j++) {
+      if (Math.abs(d.zones[j].u) > min || Math.abs(d.zones[j].v) > min) {
+        this.balls.push({
+          x: d.zones[j].x,
+          y: d.zones[j].y,
+          u: d.zones[j].u,
+          v: d.zones[j].v,
+          ox: d.zones[j].x,
+          oy: d.zones[j].y,
+          life: maxLife
+        })
+      }
+    }
+    context.drawImage(this.videoEl, 0, 0)
+    context.fillStyle = '#FFD285'
+    context.strokeStyle = '#FFD285'
+    context.save()
+    context.globalCompositeOperation = 'lighter'
+
+    for (let j = 0; j < this.balls.length; j++) {
+      var b = this.balls[j]
+      var ox = b.x
+      var oy = b.y
+      b.x -= f * 0.5 * b.u
+      b.y -= f * 0.5 * b.v
+      b.v -= f * 0.098
+      b.life -= f
+      if (b.life < 0) {
+        this.balls.splice(j, 1)
+      }
+      var a = 0.5 * Math.max(0, b.life / maxLife)
+
+      context.globalAlpha = a
       context.beginPath()
-      context.moveTo(zone.x, zone.y)
-      context.lineTo(zone.x - zone.u, zone.y + zone.v)
-      context.stroke()
-    })
+      context.arc(
+        b.x * scale,
+        b.y * scale,
+        0.05 * (maxLife - b.life),
+        0,
+        2 * Math.PI
+      )
+      context.fill()
+
+      b.ox = ox
+      b.oy = oy
+    }
+    context.restore()
   }
 
   render = () => (
     <div className="Oflow">
       <div className="Oflow_wrapper">
         <video
-          ref={el => (this.video = el)}
+          ref={el => (this.videoEl = el)}
+          style={{ display: 'none' }}
           className="Oflow_video"
           width="640px"
           height="480px"
           autoPlay
-          loop
           muted
         />
-        <canvas
-          ref={el => (this.canvas = el)}
-          className="Oflow_canvas"
-          width="640px"
-          height="480px"
-        />
+        <canvas ref={el => (this.canvasEl = el)} className="Oflow_canvas" />
       </div>
     </div>
   )
